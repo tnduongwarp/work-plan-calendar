b2dece58c6b4bcee62073e5a45d50f820b8e8dbb
duong <duong.tn204824@sis.hust.edu.vn>
2023-07-06 12:05:16 +0700

    build event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,19 @@ export class AppComponent {
     this.currentDate = new Date();
     this.populateWeekDays();
   }
-
+  ngOnInit(){
+    console.log(this.currentDate);
+    console.log(this.currentDate.getMinutes())
+    for(let i = 0; i< this.events.length; i++){
+      var event = document.getElementById(this.events[i].title);
+      console.log(event)
+      event!.style.marginLeft = this.caculateMaginLeft(this.events[i]) +'%';
+    }
+  }
   populateWeekDays(): void {
     const startOfWeek = new Date(this.currentDate);
-    console.log(startOfWeek);
-    console.log(startOfWeek.getDate());
-    console.log(startOfWeek.getDay())
+
     startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
-    console.log(startOfWeek)
     this.weekDays = [];
     for (let i = 0; i < 7; i++) {
       const day = new Date(startOfWeek);
@@ -47,4 +52,30 @@ export class AppComponent {
     nextDate.setDate(nextDate.getDate() + 7);
     this.displayCalendar(nextDate);
   }
+  checkEvent(event: any): boolean{
+    if(this.weekDays.some( day => (day.getDate() == event.start.getDate() && day.getMonth()==event.start.getMonth() && day.getFullYear()== event.start.getFullYear())))
+    return true;
+    else return false;
+  }
+  caculateMaginLeft(event: any): Number{
+    const thu = event.start.getDay();
+    const hours = event.start.getHours();
+    const mn = event.start.getMinutes();
+    if(thu == 6) return (hours*60+mn)/(24*60)*14.3;
+    else return (thu+(hours*60+mn)/(24*60))*14.3;
+  }
+   events: any = [
+    {
+      start : new Date(2023, 6, 6, 15, 0),
+      end : new Date(2023,6,7,8,30),
+      title : "first event"
+
+    },
+    {
+      start : new Date(2023, 6, 15, 15, 0),
+      end : new Date(2023,6,7,8,30),
+      title : "second event"
+
+    }
+   ]
 }
