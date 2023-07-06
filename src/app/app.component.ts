67d15c7120f8b12a7bf8dd70f20de47937204350
duong4869 <duong.tn204824@sis.hust.edu.vn>
2023-07-06 18:04:58 +0700

    current time point need

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked{
   title = 'workplancalendar';
   currentDate: Date;
   daysOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   weekDays: Date[] = [];
+  events: any = [
+    {
+      start : new Date(2023, 6, 2, 1, 0),
+      end : new Date(2023,6,4,8,30),
+      title : "third event"
+
+    },
+    {
+      start : new Date(2023, 6, 14, 15, 0),
+      end : new Date(2023,6,15,8,30),
+      title : "second event"
+
+    },
+    {
+      start : new Date(2023, 6, 6, 15, 0),
+      end : new Date(2023,6,8,8,30),
+      title : "first event"
+
+    },
+  ]
 
   constructor() {
     this.currentDate = new Date();
     this.populateWeekDays();
+    this.countLengthofEvent(this.events[0]);
+
   }
-  ngOnInit(){
-    console.log(this.currentDate);
-    console.log(this.currentDate.getMinutes())
+
+  ngAfterViewChecked(){
+
     for(let i = 0; i< this.events.length; i++){
-      var event = document.getElementById(this.events[i].title);
-      console.log(event)
-      event!.style.marginLeft = this.caculateMaginLeft(this.events[i]) +'%';
+      if(this.checkEvent(this.events[i])){
+        var event = document.getElementById(this.events[i].title);
+        event!.style.marginLeft = this.caculateMaginLeft(this.events[i]) +'px';
+        event!.style.width = this.countLengthofEvent(this.events[i]) + 'px';
+      }
+
     }
   }
   populateWeekDays(): void {
@@ -34,11 +59,13 @@ export class AppComponent {
       day.setDate(startOfWeek.getDate() + i);
       this.weekDays.push(day);
     }
+
   }
 
   displayCalendar(date: Date): void {
     this.currentDate = date;
     this.populateWeekDays();
+
   }
 
   prevWeek(): void {
@@ -52,30 +79,22 @@ export class AppComponent {
     nextDate.setDate(nextDate.getDate() + 7);
     this.displayCalendar(nextDate);
   }
+
   checkEvent(event: any): boolean{
     if(this.weekDays.some( day => (day.getDate() == event.start.getDate() && day.getMonth()==event.start.getMonth() && day.getFullYear()== event.start.getFullYear())))
     return true;
     else return false;
   }
+
   caculateMaginLeft(event: any): Number{
     const thu = event.start.getDay();
     const hours = event.start.getHours();
     const mn = event.start.getMinutes();
-    if(thu == 6) return (hours*60+mn)/(24*60)*14.3;
-    else return (thu+(hours*60+mn)/(24*60))*14.3;
+     return (thu*24+(hours+mn/60))*6;
   }
-   events: any = [
-    {
-      start : new Date(2023, 6, 6, 15, 0),
-      end : new Date(2023,6,7,8,30),
-      title : "first event"
+  countLengthofEvent(event: any){
+    const mn= event.end.getTime() - event.start.getTime();
+    return mn/(3600000)*6;
+  }
 
-    },
-    {
-      start : new Date(2023, 6, 15, 15, 0),
-      end : new Date(2023,6,7,8,30),
-      title : "second event"
-
-    }
-   ]
 }
